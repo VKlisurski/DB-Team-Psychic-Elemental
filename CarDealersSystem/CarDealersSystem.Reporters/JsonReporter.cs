96301f90357a6b3f45ae2fc2445b7c4f1fee6756
Mitko Nikolov <mitko.nikoloff@gmail.com>
2014-08-30 14:21:07 +0300

    Fixed JSON reporter
    JSON reporter now sends the data from the reports to the MySQL database.
    Redid the MySQL database so use the SalesReportsMySQLDatabase.sql in the
    folder MySQLDatabase to get it.

--- CarDealersSystem/CarDealersSystem.Reporters/JsonReporter.cs
+++ CarDealersSystem/CarDealersSystem.Reporters/JsonReporter.cs
@@ -1,6 +1,7 @@
 ﻿namespace CarDealersSystem.Reporters
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
 
@@ -13,8 +14,9 @@
 
     public class JsonReporter : IReporter
     {
-        private const string ReportsPath = "..\\..\\..\\..\\Sales reports\\";
         private const string ConnectionString = @"Server=localhost;Port=3306;Database=sales_reports_database;Uid=root;Pwd=12345;";
+        private const string ReportsPath = "..\\..\\..\\..\\Sales reports\\";
+        private const string ReportsFileExtension = ".json";
 
         public void Report()
         {
@@ -25,57 +27,115 @@
 
             var dbContext = new CarDealersSystemDbContext();
 
-            var reports = dbContext.Cars
-                .Join(dbContext.SalesReports, sale => sale.CarID, car => car.CarID,
-                    (car, sale) => new JsonReport
-                        {
-                            Id = car.CarID,
-                            CarModer = car.ModelName,
-                            CarMake = car.MakeName,
-                            PricePerUnit = car.Price,
-                            Quantity = dbContext.SalesReports
-                                .Where(rep => rep.CarID == car.CarID)
-                                .Sum(profit => profit.Quantity),
-                            Sum = dbContext.SalesReports
-                                .Where(rep => rep.CarID == car.CarID)
-                                .Sum(profit => profit.Sum)
-                        });
+            var reports = JsonReporter.GetJsonReports(dbContext);
+
+            var saveFilesPaths = JsonReporter.SaveReportsToFiles(reports);
+
+            JsonReporter.SendReportToDatabase(reports, saveFilesPaths);
+        }
+
+        private static IQueryable<JsonReport> GetJsonReports(CarDealersSystemDbContext dbContext)
+        {
+            var cars = dbContext.Cars;
+            var sales = dbContext.SalesReports;
+
+            var reports = cars.Join(sales, car => car.CarID, sale => sale.CarID,
+                                         (car, sale) => new JsonReport
+                                         {
+                                             Id = car.CarID,
+                                             CarModer = car.ModelName,
+                                             CarMake = car.MakeName,
+                                             PricePerUnit = car.Price,
+                                             Quantity = sales.Where(rep => rep.CarID == car.CarID)
+                                                 .Sum(profit => profit.Quantity),
+                                             Sum = sales.Where(rep => rep.CarID == car.CarID)
+                                                 .Sum(profit => profit.Sum)
+                                         });
+
+            return reports;
+        }
+
+        private static List<string> SaveReportsToFiles(IQueryable<JsonReport> reports)
+        {
+            List<string> reportsFilePaths = new List<string>();
 
             foreach (var report in reports)
             {
-                string serialized = JsonConvert.SerializeObject(report, Formatting.Indented);
+                string reportFilePath = string.Format("{0}{1}{2}", ReportsPath, report.Id, ReportsFileExtension);
 
-                string reportFilePath = string.Format("{0}{1}.json", ReportsPath, report.Id);
+                StreamWriter writer = new StreamWriter(reportFilePath);
 
-                using (StreamWriter writer = new StreamWriter(reportFilePath))
+                using (writer)
                 {
+                    string serialized = JsonConvert.SerializeObject(report, Formatting.Indented);
+
                     writer.WriteLine(serialized);
-                    //this.InsertReportIntoDatabase(report, reportFilePath);
+
+                    reportsFilePaths.Add(reportFilePath);
+                }
+            }
+
+            return reportsFilePaths;
+        }
+
+        private static void SendReportToDatabase(IQueryable<JsonReport> reportsQuery, List<string> saveFilesPaths)
+        {
+            var reports = reportsQuery.ToList<JsonReport>();
+
+            for (int i = 0, length = reports.Count; i < length; i++)
+            {
+                JsonReport report = reports[i];
+                string reportFilePath = saveFilesPaths[i];
+
+                MySqlConnection connection = new MySqlConnection(ConnectionString);
+
+                using (connection)
+                {
+                    connection.Open();
+
+                    string commandText = JsonReporter.GetCommandText(connection, reportFilePath);
+
+                    MySqlCommand command = new MySqlCommand(commandText, connection);
+
+                    command.Parameters.AddWithValue("@carId", report.Id);
+                    command.Parameters.AddWithValue("@filePath", reportFilePath);
+                    command.Parameters.AddWithValue("@carModel", report.CarModer);
+                    command.Parameters.AddWithValue("@carMake", report.CarMake);
+                    command.Parameters.AddWithValue("@quantity", report.Quantity);
+                    command.Parameters.AddWithValue("@totalIncome", report.Sum);
+
+                    command.ExecuteNonQuery();
                 }
             }
         }
 
-        private void InsertReportIntoDatabase(JsonReport report, string reportFilePath)
+        private static string GetCommandText(MySqlConnection connection, string reportFilePath)
         {
-            MySqlConnection connection = new MySqlConnection(ConnectionString);
+            string returnCommand = "INSERT INTO sales_reports(CarId, SalesReportFilePath, CarModel, CarMake, QuantitySold, TotalIncome) " +
+                                   "VALUES (@carId, @filePath, @carModel, @carMake, @quantity, @totalIncome);";
 
-            string commandText = "INSERT INTO sales_reports(car-id, sales-report-file-path, car-model, car-make, quantity-sold, total-income) " +
-                    "VALUES (@carId, @filePath, @carModel, @carMake, @quantity, @totalIncome);";
+            string checkReportFileExistanceCommand = "SELECT SalesReportFilePath FROM sales_reports";
 
-            MySqlCommand command = new MySqlCommand(commandText, connection);
-            using (connection)
+            MySqlCommand reportFileCheckCommand = new MySqlCommand(checkReportFileExistanceCommand, connection);
+            var reader = reportFileCheckCommand.ExecuteReader();
+
+            using (reader)
             {
-                connection.Open();
+                while (reader.Read())
+                {
+                    string rowValue = (string)reader["SalesReportFilePath"];
 
-                command.Parameters.AddWithValue("@carId", report.Id);
-                command.Parameters.AddWithValue("@filePath", reportFilePath);
-                command.Parameters.AddWithValue("@carModel", report.CarModer);
-                command.Parameters.AddWithValue("@carMake", report.CarMake);
-                command.Parameters.AddWithValue("@quantity", report.Quantity);
-                command.Parameters.AddWithValue("@totalIncome", report.Sum);
-
-                command.ExecuteNonQuery();
+                    if (rowValue == reportFilePath)
+                    {
+                        returnCommand = "UPDATE sales_reports " +
+                                        "SET QuantitySold = @quantity, TotalIncome = @totalIncome " +
+                                        "WHERE SalesReportFilePath = @filePath;";
+                        break;
+                    }
+                }
             }
+
+            return returnCommand;
         }
     }
 }
